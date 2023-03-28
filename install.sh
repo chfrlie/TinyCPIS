251e2a0d8c35dfb756b8c7a425b2f975df388e5e
chfrlie <128047351+chfrlie@users.noreply.github.com>
2023-03-28 05:59:02 +0100

    Updates to make it work.
    Ubuntu Server terminal wouldn't accept it, I have diagnosed it and it now should be working.

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
-echo Hello! Thanks for using chfrlie's install script on GitHub, We will now install TinyCP for you. Please enter the password you want for the control panel when it asks.
-
-
-bash
-
+Hello! Thanks for using chfrlies install script on GitHub, We will now install TinyCP for you. Please enter the password you want for the control panel when it asks.
 sudo apt install gnupg ca-certificates
-
 sudo apt-key adv --fetch-keys http://repos.tinycp.com/ubuntu/conf/gpg.key
-
 sudo echo "deb http://repos.tinycp.com/ubuntu all main" | sudo tee /etc/apt/sources.list.d/tinycp.list
-
 sudo apt-get update
-
 sudo apt-get install tinycp
-
 exit
-
 You now have TinyCP installed, For any support options, Go to TinyCP as I only make this script. (Copyrighted by Charlie Walsh @chfrlie on github.) 
